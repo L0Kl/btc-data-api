0e527c9e8dbbc187c94befd6f6951807d8492aec
L0Kl <eggertleon@gmail.com>
2025-07-17 23:11:07 +0200

    Initialize project with Maven wrapper, configuration files, and basic Spring Boot application structure

---
+++ src/main/java/com/example/btc_data_api/BtcDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.btc_data_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtcDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtcDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/btc_data_api/BtcDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.btc_data_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtcDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
